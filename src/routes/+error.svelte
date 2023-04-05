7264eab5300d2b868b748b31e8fd43bf075042fc
harshmangalam <harshdev8218@gmail.com>
2023-04-05 21:47:55 +0530

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { error } from '@sveltejs/kit';
+</script>
+
+<section class="min-h-screen grid place-items-center">
+	<div class="max-w-md w-full mx-auto text-center flex flex-col space-y-4">
+		<h1 class="text-2xl font-bold">Oops!</h1>
+		<p class="text-gray-600">Sorry, an unexpected error has occurred.</p>
+		<p>
+			<i class="text-gray-400">{$page.error?.message}</i>
+		</p>
+	</div>
+</section>
