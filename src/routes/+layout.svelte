c9baad583e3613189a0c864364c17171de7dff4c
harshmangalam <harshdev8218@gmail.com>
2023-04-05 21:42:01 +0530

    updated layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
 	import '../app.postcss';
+	import Sidebar from '../lib/components/sidebar.svelte';
 </script>
 
-<slot />
+<div class="flex">
+	<!-- sidebar  -->
+	<Sidebar />
+	<main class="flex-1">
+		<slot />
+	</main>
+</div>
