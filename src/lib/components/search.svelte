585b060357d7e877f5d22b35d56802be166f5a6d
harshmangalam <harshdev8218@gmail.com>
2023-04-05 21:41:13 +0530

    added search component

---
+++ src/lib/components/search.svelte
@@ -0,0 +1,15 @@
+<script>
+	import SearchIcon from './icons/search-icon.svelte';
+</script>
+
+<form class="group relative flex-1">
+	<SearchIcon
+		iconClass="absolute w-5 h-5 left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500"
+	/>
+	<input
+		class="rounded-md py-2 pl-10 w-full border-gray-200 shadow"
+		type="text"
+		aria-label="Search"
+		placeholder="Search"
+	/>
+</form>
