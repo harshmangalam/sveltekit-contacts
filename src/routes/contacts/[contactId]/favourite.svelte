030dfeb1987a84573a9ddde528fa1ab40f77a860
harshmangalam <harshdev8218@gmail.com>
2023-04-05 22:07:30 +0530

    added star solid icons

---
+++ src/routes/contacts/[contactId]/favourite.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	let favorite = false;
+</script>
+
+<form method="post" use:enhance>
+	<button
+		name="favorite"
+		value={favorite ? 'false' : 'true'}
+		aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
+	>
+		{favorite ? '★' : '☆'}
+	</button>
+</form>
