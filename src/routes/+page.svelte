5afdbd339ac9be28a1b951e2647809c412a94677
harshmangalam <harshdev8218@gmail.com>
2023-04-06 19:15:50 +0530

    added index route content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="min-h-screen grid place-items-center">
+	<div class="max-w-md w-full mx-auto text-center flex flex-col space-y-4">
+		<img src="https://kit.svelte.dev/favicon.png" class="w-16 h-16 mx-auto" loading="lazy" alt="" />
+
+		<div class="text-gray-500">
+			<p>This is a demo for Sveltekit</p>
+			<p>
+				Check out the docs at <a
+					href="https://kit.svelte.dev/"
+					target="_blank"
+					class="text-blue-500">https://kit.svelte.dev</a
+				>
+			</p>
+		</div>
+	</div>
+</section>
