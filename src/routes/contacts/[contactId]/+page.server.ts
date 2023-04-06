a7b5959d917f3612e66e81cf7e03abe99c405858
harshmangalam <harshdev8218@gmail.com>
2023-04-06 21:23:24 +0530

    fetch contact by id

---
+++ src/routes/contacts/[contactId]/+page.server.ts
@@ -0,0 +1,17 @@
+import { prisma } from '$lib/server/prisma';
+import { fail } from '@sveltejs/kit';
+
+export async function load({ params }) {
+	try {
+		const contact = await prisma.contact.findUnique({
+			where: {
+				id: params.contactId
+			}
+		});
+		return {
+			contact
+		};
+	} catch (error) {
+		return fail(500);
+	}
+}
