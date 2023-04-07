dc5af938e7fa04e23177b51bed9dd97541761b4d
harshmangalam <harshdev8218@gmail.com>
2023-04-07 12:47:48 +0530

    added resposive layout

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,17 +1,23 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
+	import ChevronLeftIcon from '$lib/icons/chevron-left-icon.svelte';
 	import Search from './search.svelte';
+
+	export let showDrawer: boolean;
 </script>
 
-<aside class="hidden max-w-xs w-full md:flex flex-col divide-y bg-gray-100 border-r h-screen">
-	<header class="py-2 px-6 flex items-center gap-2">
+<aside
+	class:hidden={!showDrawer}
+	class="absolute md:static md:max-w-xs w-full flex flex-col divide-y bg-gray-100 border-r h-screen"
+>
+	<header class="py-2 px-2 md:px-6 flex items-center gap-2">
 		<Search />
 		<a href="/contacts/new" class="bg-white border text-blue-500 shadow px-2 py-2 rounded-md">
 			New
 		</a>
 	</header>
 
-	<section class="flex-1 overflow-y-auto px-6 py-4">
+	<section class="flex-1 overflow-y-auto px-2 md:px-6 py-4">
 		<ul class="h-full flex flex-col space-y-2">
 			{#each $page?.data?.contacts as contact}
 				<li>
@@ -31,8 +37,11 @@
 		</ul>
 	</section>
 
-	<footer class="py-4 flex space-x-2 items-center px-6">
-		<img src="https://kit.svelte.dev/favicon.png" class="w-6 h-6" loading="lazy" alt="" />
-		<span class="font-medium text-sm">Sveltekit Contacts</span>
+	<footer class="py-4 flex space-x-2 items-center px-2 md:px-6">
+		<img src="https://kit.svelte.dev/favicon.png" class="w-6 h-6 flex-none" loading="lazy" alt="" />
+		<span class="font-medium text-sm flex-1">Sveltekit Contacts</span>
+		<button class="block md:hidden">
+			<ChevronLeftIcon />
+		</button>
 	</footer>
 </aside>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script>
+	import MenuIcons from '$lib/icons/menu-icons.svelte';
 	import '../app.postcss';
 	import Sidebar from '../lib/components/sidebar.svelte';
+
+	let showDrawer = false;
 </script>
 
 <div class="flex">
 	<!-- sidebar  -->
-	<Sidebar />
+	<Sidebar {showDrawer} />
+	<div class="block md:hidden absolute top-4 left-4">
+		<button on:click={() => (showDrawer = !showDrawer)}>
+			<MenuIcons />
+		</button>
+	</div>
 	<main class="flex-1">
 		<slot />
 	</main>
