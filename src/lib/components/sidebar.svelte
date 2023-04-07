178bac6659671c00f5359d11bb511d0be2942b10
harshmangalam <harshdev8218@gmail.com>
2023-04-07 13:02:47 +0530

    added mobile sidebar

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import ChevronLeftIcon from '$lib/icons/chevron-left-icon.svelte';
+	import { getContext } from 'svelte';
 	import Search from './search.svelte';
 
-	export let showDrawer: boolean;
+	const drawer = getContext('drawer');
 </script>
 
 <aside
-	class:hidden={!showDrawer}
-	class="absolute md:static md:max-w-xs w-full flex flex-col divide-y bg-gray-100 border-r h-screen"
+	class={`${
+		!$drawer ? 'hidden md:flex' : ''
+	} absolute md:static md:max-w-xs w-full flex flex-col divide-y bg-gray-100 border-r h-screen`}
 >
-	<header class="py-2 px-2 md:px-6 flex items-center gap-2">
+	<header class="z-10 py-2 px-2 md:px-6 flex items-center gap-2">
 		<Search />
 		<a href="/contacts/new" class="bg-white border text-blue-500 shadow px-2 py-2 rounded-md">
 			New
@@ -40,7 +42,7 @@
 	<footer class="py-4 flex space-x-2 items-center px-2 md:px-6">
 		<img src="https://kit.svelte.dev/favicon.png" class="w-6 h-6 flex-none" loading="lazy" alt="" />
 		<span class="font-medium text-sm flex-1">Sveltekit Contacts</span>
-		<button class="block md:hidden">
+		<button class="block md:hidden" on:click={() => ($drawer = false)}>
 			<ChevronLeftIcon />
 		</button>
 	</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,19 @@
 	import MenuIcons from '$lib/icons/menu-icons.svelte';
 	import '../app.postcss';
 	import Sidebar from '../lib/components/sidebar.svelte';
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
 
-	let showDrawer = false;
+	const drawer = writable(false);
+
+	setContext('drawer', drawer);
 </script>
 
 <div class="flex">
 	<!-- sidebar  -->
-	<Sidebar {showDrawer} />
+	<Sidebar />
 	<div class="block md:hidden absolute top-4 left-4">
-		<button on:click={() => (showDrawer = !showDrawer)}>
+		<button on:click={() => ($drawer = true)}>
 			<MenuIcons />
 		</button>
 	</div>
