e1ecf2426502f9c5f5b27e407b485a03c314c66c
harshmangalam <harshdev8218@gmail.com>
2023-04-07 12:15:22 +0530

    added edit handler

--- src/routes/contacts/[contactId]/edit/+page.server.ts
+++ src/routes/contacts/[contactId]/edit/+page.server.ts
@@ -1,5 +1,5 @@
 import { prisma } from '$lib/server/prisma.js';
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import { zfd } from 'zod-form-data';
 import * as z from 'zod';
 import type { inferFlattenedErrors } from 'zod';
@@ -29,3 +29,28 @@ export async function load({ params, request }) {
 		contact
 	};
 }
+
+export const actions = {
+	async default({ params, request }) {
+		const formData = await request.formData();
+		const fields = Object.fromEntries(formData) as z.infer<typeof contactSchema>;
+		const result = contactSchema.safeParse(formData);
+
+		if (!result.success) {
+			const data = {
+				fields,
+				errors: (result.error.flatten() as inferFlattenedErrors<typeof contactSchema>).fieldErrors
+			};
+			return fail(422, data);
+		}
+
+		await prisma.contact.update({
+			where: {
+				id: params.contactId
+			},
+			data: fields
+		});
+
+		throw redirect(303, `/contacts/${params.contactId}`);
+	}
+};
--- src/routes/contacts/[contactId]/edit/+page.svelte
+++ src/routes/contacts/[contactId]/edit/+page.svelte
@@ -11,33 +11,33 @@
 		<div class="grid grid-cols-2 gap-4 max-w-md mx-auto">
 			<section class="col-span-1 flex flex-col space-y-2">
 				<label for="firstName">First name</label>
-				<input value={form?.fields?.firstName ?? ''} type="text" name="firstName" id="firstName" />
+				<input value={data?.contact?.firstName ?? ''} type="text" name="firstName" id="firstName" />
 				{#if firstNameError}
 					<span class="text-red-500 text-sm">{firstNameError}</span>
 				{/if}
 			</section>
 			<section class="col-span-1 flex flex-col space-y-2">
 				<label for="lastName">Last name</label>
-				<input value={form?.fields?.lastName ?? ''} type="text" name="lastName" id="lastName" />
+				<input value={data?.contact?.lastName ?? ''} type="text" name="lastName" id="lastName" />
 			</section>
 			<section class="col-span-2 flex flex-col space-y-2">
 				<label for="twitter">Twitter</label>
-				<input value={form?.fields?.twitter ?? ''} type="text" name="twitter" id="twitter" />
+				<input value={data?.contact?.twitter ?? ''} type="text" name="twitter" id="twitter" />
 			</section>
 			<section class="col-span-2 flex flex-col space-y-2">
 				<label for="avatarUrl">Avatar URL</label>
-				<input value={form?.fields?.avatarUrl ?? ''} type="text" name="avatarUrl" id="avatarUrl" />
+				<input value={data?.contact?.avatarUrl ?? ''} type="text" name="avatarUrl" id="avatarUrl" />
 			</section>
 			<section class="col-span-2 flex flex-col space-y-2">
 				<label for="notes">Notes</label>
-				<textarea value={form?.fields?.notes ?? ''} name="notes" id="notes" rows="5" />
+				<textarea value={data?.contact?.notes ?? ''} name="notes" id="notes" rows="5" />
 			</section>
 
 			<section class="flex space-x-2 items-center">
 				<button
 					class="bg-white border text-blue-500 shadow px-2 py-2 rounded-md disabled:bg-gray-100 disabled:text-gray-500"
 				>
-					Save
+					Edit
 				</button>
 				<a href="/" class="bg-white border text-red-500 shadow px-2 py-2 rounded-md"> Cancel </a>
 			</section>
