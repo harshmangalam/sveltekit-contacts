97fa34666ce720f59512fe7f0365d958dc896ced
harshmangalam <harshdev8218@gmail.com>
2023-04-07 14:28:48 +0530

    added loading state for favorite

--- src/routes/contacts/[contactId]/favourite.svelte
+++ src/routes/contacts/[contactId]/favourite.svelte
@@ -4,10 +4,24 @@
 	import StarSolidIcon from '$lib/icons/star-solid-icon.svelte';
 
 	export let favorite = false;
+
+	let loading = false;
 </script>
 
-<form method="post" use:enhance action="?/favorite">
+<form
+	method="post"
+	use:enhance={() => {
+		loading = true;
+		return async ({ update }) => {
+			loading = false;
+			await update();
+		};
+	}}
+	action="?/favorite"
+>
 	<button
+		disabled={loading}
+		class="disabled:cursor-wait"
 		name="favorite"
 		value={favorite ? 'false' : 'true'}
 		aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
